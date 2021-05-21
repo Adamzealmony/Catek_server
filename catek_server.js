9767b433f7a0ce139a3de5b8cca8bb113b4476c0
吴垠劼 <wuyinjie@vargo.com.cn>
2021-05-21 17:07:06 +0800

    initial

---
+++ catek_server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var history = require('connect-history-api-fallback');
+
+var app = express();
+
+app.use(history());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static('public'));
+
+
+app.listen(3713, function () {
+    console.log('Example app listening on port 3713!');
+});
+
+// // 利用express.static中间件来托管静态资源。
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.listen(8888, () => {
+//     console.log('start: 8888')
+// })
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/index.html'))
+// })
+
+// app.get('*.css', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/style.css'))
+// })
