7b9afcf8a0a14b15a0685d00e36510f8efec157a
吴垠劼 <wuyinjie@vargo.com.cn>
2021-05-24 13:49:41 +0800

    update

--- catek_server.js
+++ catek_server.js
@@ -14,6 +14,7 @@ app.listen(3713, function () {
     console.log('Example app listening on port 3713!');
 });
 
+
 // // 利用express.static中间件来托管静态资源。
 // app.use(express.static(path.join(__dirname, 'public')));
 
